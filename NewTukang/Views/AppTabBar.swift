f46f87bbf405b2dbbde59228f562704ccc2ad29e
Jonathan Ng <iamjonathanng6657@gmail.com>
2020-08-21 10:54:32 +0800

    Initial Commit

---
+++ NewTukang/Views/AppTabBar.swift
@@ -0,0 +1,21 @@
+//
+//  AppTabBar.swift
+//  NewTukang
+//
+//  Created by Jonathan Ng on 21/08/2020.
+//  Copyright © 2020 Jonathan Ng. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTabBar()
+    }
+}
